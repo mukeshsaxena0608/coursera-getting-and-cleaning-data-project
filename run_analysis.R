476f4b08466cff858c4ed91b209c77048d0bc544
mukeshsaxena0608 <35054124+mukeshsaxena0608@users.noreply.github.com>
2018-07-03 23:50:17 +0530

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -7,8 +7,9 @@ if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
   download.file(fileURL, filename, method="curl")
 }  
-if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
+if (!file.exists("UCI HAR Dataset")) {   
+ 
+     unzip(filename) 
 }
 
 # Load activity labels + features
